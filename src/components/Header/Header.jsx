d5c161ffea94c95c162b23496716c05393bd6077
Nitin <nitin2000mittal.com>
2023-07-09 15:13:49 +0530

    added icons to header, completed header structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,40 @@
+import {HiOutlineShoppingBag} from 'react-icons/hi';
+import {AiOutlineHeart} from 'react-icons/ai';
+
 import "./Header.css";
 
-export function Header(){
-    return(
-       <>
-        <div className="header-main">
-            <div className="announcement-bar">
-                <div className="announcement-text">
-                    <p>
-                        Groove to your favourite tunes with <b>Rockerz 235 V2</b> -- now at <b>₹899.</b> 💥 Shop now!
-                    </p>
-                </div>
-            </div>
-            <div className="nav-bar">
-                <div className="nav-left">
-                    <img src="https://www.boat-lifestyle.com/cdn/shop/files/boAt_logo_small.svg?v=1682421543" alt="logo"/>
-                </div>
-                <div class="nav-right">
-                    <div className="search"><input type="text" placeholder="Search"/></div>
-                    <div>Login(icon)</div>
-                    <div>Cart(icon)</div>
-                    <div>Wishlist(icon)</div>
-                </div>
-            </div>
+export function Header() {
+  return (
+    <>
+      <div className="header-main">
+        <div className="announcement-bar">
+          <div className="announcement-text">
+            <p>
+              Groove to your favourite tunes with <b>Rockerz 235 V2</b> -- now
+              at <b>₹899.</b> 💥 Shop now!
+            </p>
+          </div>
         </div>
-       </>
-    )
+        <div className="nav-bar">
+          <div className="nav-left">
+            <img
+              src="https://www.boat-lifestyle.com/cdn/shop/files/boAt_logo_small.svg?v=1682421543"
+              alt="logo"
+            />
+          </div>
+          <div class="nav-right">
+            <div className="search">
+              <img src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/search_558f3ad8-0ef9-4a9b-8576-a396b44aacd8.png?v=1680091847" alt="search-icon"/>
+              <input type="text" placeholder="Search" />
+            </div>
+            <div className="login">
+              <img src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/Profile-1.png?v=1663762989" alt="login" />
+            </div>
+            <div className="cart"><HiOutlineShoppingBag/></div>
+            <div className="wishlist"><AiOutlineHeart/></div> 
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
